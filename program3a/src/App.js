b00140d8049882d0f5cd7682012d52ad0fcff730
Nani Roy <Nani0634>
2026-07-08 21:45:40 +0530

    first commit

---
+++ program3a/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react"; 
+ 
+class App extends Component { 
+ 
+  constructor() { 
+    super(); 
+ 
+    this.state = { 
+      count: 0 
+    }; 
+  } 
+ 
+  increment = () => { 
+    this.setState({ 
+      count: this.state.count + 1 
+    }); 
+  }; 
+ 
+  render() { 
+    return ( 
+      <div> 
+        <h1>Class Counter</h1> 
+ 
+        <h2>{this.state.count}</h2> 
+Page No - 47                                                                                                                                      FSD – II LAB MANUAL 
+ 
+        <button onClick={this.increment}> 
+          Increment 
+        </button> 
+      </div> 
+    ); 
+  } 
+} 
+ 
+export default App;

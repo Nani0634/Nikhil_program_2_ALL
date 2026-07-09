04331df3faab7af85410a0ea750fa660b437c473
Nani Roy <Nani0634>
2026-07-09 19:32:16 +0530

    Updated Program 3d

--- program3d/src/App.js
+++ program3d/src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react"; 
-function App() { 
-const [loggedIn, setLoggedIn] = useState(false); 
-return ( 
-<div> 
-<h1>Conditional Rendering</h1> 
-{ 
-} 
-loggedIn 
-? <h2>Welcome User</h2> 
-: <h2>Please Login</h2><button 
-onClick={() => setLoggedIn(!loggedIn)} 
-> 
-Toggle 
-</button> 
-</div> 
-); 
-} 
+import React, { useState } from "react";
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <div>
+      <h1>Conditional Rendering</h1>
+
+      <p>loggedIn ?</p>
+
+      {loggedIn ? (
+        <h2>Welcome User</h2>
+      ) : (
+        <h2>Please Login</h2>
+      )}
+
+      <button onClick={() => setLoggedIn(!loggedIn)}>
+        Toggle
+      </button>
+    </div>
+  );
+}
+
 export default App;
